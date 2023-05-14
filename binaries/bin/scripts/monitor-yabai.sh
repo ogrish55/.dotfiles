345bf4f13c4626f11ed15bf910b00d7ca912ad1f
wexokk <kk@wexo.dk>
2023-05-14 15:45:39 +0200

    check for no results

--- binaries/bin/scripts/monitor-yabai.sh
+++ binaries/bin/scripts/monitor-yabai.sh
@@ -1,6 +1,12 @@
-resolution=$(system_profiler SPDisplaysDataType |grep Resolution | head -n 1 | awk '{print $2}')
+resolution_width=$(system_profiler SPDisplaysDataType |grep Resolution | head -n 1 | awk '{print $2}')
 
-if [ $((resolution)) -gt 2560 ]; then
+if [ -z "$resolution_width" ]; then
+  echo "No resolution width found."
+  echo "Possible bug in: /Users/wexokk/.dotfiles"
+  exit 0
+fi
+
+if [ $((resolution_width)) -gt 2560 ]; then
   yabai -m config left_padding 50
   yabai -m config right_padding 50
 else
