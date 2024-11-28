a6f1ddfc762e88b448419e61441c97b02f2f7c57
wexokk <kk@wexo.dk>
2024-11-28 08:28:08 +0100

    font size kitty and nvim changes

--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -5,7 +5,6 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'center on going up'})
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'center on going down'})
 vim.keymap.set("n", "n", "nzzzv", { desc = 'center on next search'})
 vim.keymap.set("n", "N", "Nzzzv", { desc = 'center on next search'})
-vim.keymap.set("n", "<leader>s", "/", { desc = 'quick search in file - should remove..'})
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move visually selected down'})
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move visually selected up'})
@@ -15,12 +14,9 @@ vim.keymap.set("v", "<leader>y", "\"*y", { desc = 'yank to clipboard'})
 vim.keymap.set("n", "<leader>Y", "\"*Y", { desc = 'yank to clipboard'})
 
 vim.api.nvim_set_keymap("n", "<space>en", ":lua require('utils').edit_neovim()<CR>", { noremap = true , desc = 'quickly edit neovim config files'})
-vim.api.nvim_set_keymap("n", "<leader>ee", "<Cmd>lua require('telescope').extensions.frecency.frecency()<CR>", {noremap = true, silent = true, desc = 'recent files' })
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
-vim.keymap.set("n", "<leader>fd", vim.cmd.Ex, { desc = 'file explorer'})
-vim.keymap.set("n", "<leader>fd", ":lua require('nvim-tree.api').tree.toggle()<CR>", { desc = 'file explorer'})
+-- vim.keymap.set("n", "<leader>fd", vim.cmd.Ex, { desc = 'file explorer'})
+vim.keymap.set("n", "<leader>dd", ":lua require('nvim-tree.api').tree.toggle()<CR>", { desc = 'file explorer'})
 
 
 vim.keymap.set("n", "<leader>Y", "\"*Y", { desc = 'yank to clipboard'})
